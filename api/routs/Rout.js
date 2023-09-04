61bef2744ee94a743139c46b75b9aa01ad3ff584
MOSA ISSA <mosasenio@gmail.com>
2023-09-04 23:36:30 +0300

    param test

--- api/routs/Rout.js
+++ api/routs/Rout.js
@@ -3,14 +3,14 @@ const UserRouter = require("../Users/rout/UserRoute");
 const GameDataRouter = require("./GameDataRoute");
 const localRout = require("./localRout");
 const GamesDataRouter = require("./GamesDataRoute");
-const GenreRout = require("../Games/0-params/ganre/rout/genreRout");
+const genreRout = require("../Games/aParams/ganre/rout/genreRout");
 
 
 const Routs =  [
     UserRouter,
     // GameDataRouter,
     localRout,
-    GenreRout,
+    genreRout,
     // GamesDataRouter,
  ] ;
 
